6e38dda1cc04820b1c5e7f31ec4c45b2295be6ce
akosnad <akos@nadudvari.org>
2024-03-19 16:15:26 +0100

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "akosnad's neovim config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neovim = {
+      url = "github:neovim/neovim/stable?dir=contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, neovim }: 
+  let
+    overlayFlakeInputs = prev: final: {
+      neovim = neovim.packages.x86_64-linux.neovim;
+    };
+
+    overlayNeovim = prev: final: {
+      neovim = import ./packages/neovim.nix {
+        pkgs = final;
+      };
+    };
+
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [ overlayFlakeInputs overlayNeovim ];
+    };
+  in {
+    packages.x86_64-linux.default = pkgs.neovim;
+    apps.x86_64-linux.default = {
+      type = "app";
+      program = "${pkgs.neovim}/bin/nvim";
+    };
+  };
+}
