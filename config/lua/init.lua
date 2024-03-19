9f390e9c7542d812c411bff206ba59adbd9ebcd0
akosnad <akos@nadudvari.org>
2024-03-19 17:00:18 +0100

    lua config

---
+++ config/lua/init.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
--- packages/neovim.nix
+++ packages/neovim.nix
@@ -2,9 +2,21 @@
 let
   customRC = import ../config { inherit pkgs; };
   plugins = import ../plugins.nix { inherit pkgs; };
-in pkgs.wrapNeovim pkgs.neovim {
-      configure = {
-        inherit customRC;
-        packages.all.start = plugins;
-      };
-    }
+  runtimeDeps = import ../runtimeDeps.nix { inherit pkgs; };
+  neovimRuntimeDeps = pkgs.symlinkJoin {
+    name = "neovim-runtime-deps";
+    paths = runtimeDeps;
+  };
+  neovimUnwrapped = pkgs.wrapNeovim pkgs.neovim {
+    configure = {
+      inherit customRC;
+      packages.all.start = plugins;
+    };
+  };
+in pkgs.writeShellApplication {
+  name = "nvim";
+  runtimeInputs = [ neovimRuntimeDeps ];
+  text = ''
+    ${neovimUnwrapped}/bin/nvim "$@"
+  '';
+}
